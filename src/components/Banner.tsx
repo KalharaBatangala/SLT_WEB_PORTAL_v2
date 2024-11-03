c31788a5b874375a666785fd22aa5429215dcfbd
KalanaNiyumal <shkniyumal@gmail.com>
2024-11-03 22:16:56 +0530

    change banner images

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,15 +1,48 @@
-import Box from '@mui/material/Box'
-import React from 'react'
-import BannerImage from '../assets/images/banner.png'
+import Box from '@mui/material/Box';
+import React, { useEffect, useState } from 'react';
+import BannerImage1 from '../assets/images/banner1.jpg';
+import BannerImage2 from '../assets/images/banner2.jpg';
+import BannerImage3 from '../assets/images/banner3.jpg';
 
+const images = [BannerImage1, BannerImage2, BannerImage3];
 
 const Banner = () => {
-  return (
-    <Box sx={{width:"100%",position:"relative",bottom:"10px"}}
-    >
-        <img width="100%" src={BannerImage}/>
-    </Box>
-  )
-}
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-export default Banner
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        gap: 1.5,
+        flexDirection: "column",
+        alignItems: "center",
+        padding: 1,
+        borderRadius: "10px",
+        position: "relative",
+        height: "195px",
+        boxShadow: "0px 3px 3px #0000004A",
+        overflow: "hidden",
+        margintop: 3,
+        backgroundImage: `url(${images[currentImageIndex]})`,
+        backgroundSize: 'cover', // Cover the entire box
+        backgroundPosition: 'center', // Center the image
+        backgroundRepeat: 'no-repeat', // Prevent the image from repeating
+        mt: 1,
+      }}
+    >
+    </Box>
+  );
+};
+
+export default Banner;
+
+
+
